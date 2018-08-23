f56d209e98cd61b64da88c88614dfc36778ab516
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2018-08-23 13:15:55 +0530

    Add files via upload

---
+++ moush.js
@@ -0,0 +1,36 @@
+var imageBox = document.getElementById("imageBox");   
+var editTextBox = document.getElementById("editTextBox"); 
+var paintBox = document.getElementById("paintBox");  
+var eraseBox = document.getElementById("eraseBox"); 
+
+
+
+
+
+
+
+function doMouseDown(e){
+
+    if (imageBox.checked) imageMouseDown(e);
+    else if (editTextBox.checked) textMouseDown(e);
+    
+    else dragStart(e);
+}
+
+function doMouseMove(e){
+    if (imageBox.checked)  imageMouseMove(e);
+    else if (editTextBox.checked) textMouseMove(e);
+    else drag(e);
+}
+
+function doMouseUp(e){
+    if (imageBox.checked)  imageMouseUp(e);
+    else if (editTextBox.checked) textMouseUp(e);
+    else dragStop(e);
+}
+
+function doMouseOut(e){
+    if (imageBox.checked) 
+	imageMouseOut(e);
+    else if (editTextBox.checked) textMouseOut(e);
+}
