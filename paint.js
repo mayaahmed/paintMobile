beab8600312641ca5de33119be791228866e48b0
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2018-08-23 22:20:25 +0530

    Add files via upload

--- paint.js
+++ paint.js
@@ -243,13 +243,11 @@ function init() {
     context.lineWidth = lineWidth.value;
 
 
-  /*  canvas.addEventListener('mousedown', doMouseDown, false);
+    canvas.addEventListener('mousedown', doMouseDown, false);
     canvas.addEventListener('mousemove', doMouseMove, false);
-    canvas.addEventListener('mouseup',doMouseUp, false); */
+    canvas.addEventListener('mouseup',doMouseUp, false);
    
-    canvas.addEventListener('touchstart', doMouseDown, false);
-    canvas.addEventListener('touchmove', doMouseMove, false);
-    canvas.addEventListener('touchend',doMouseUp, false);
+    
 
 
     lineWidth.addEventListener("input", changeLineWidth, false);
@@ -264,11 +262,59 @@ function init() {
     
     clearCanvas.addEventListener('click', eraseCanvas, false);
     takeSnapshot();
-}
 
+// Set up touch events for mobile, etc
+canvas.addEventListener("touchstart", function (e) {
+        mousePos = getTouchPos(canvas, e);
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousedown", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+  var mouseEvent = new MouseEvent("mouseup", {});
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+  var touch = e.touches[0];
+  var mouseEvent = new MouseEvent("mousemove", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+  });
+  canvas.dispatchEvent(mouseEvent);
+}, false);
+
+}
 
 window.addEventListener('load', init, false);
 
 
 
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+  var rect = canvasDom.getBoundingClientRect();
+  return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+  };
+}
 
+
+// Prevent scrolling when touching the canvas
+document.body.addEventListener("touchstart", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
+document.body.addEventListener("touchmove", function (e) {
+  if (e.target == canvas) {
+    e.preventDefault();
+  }
+}, false);
