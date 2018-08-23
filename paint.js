5f0b1bff8133e8f30ca0c62b0103b6a23acdbed7
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2018-08-23 21:17:12 +0530

    Add files via upload

--- paint.js
+++ paint.js
@@ -252,12 +252,12 @@ function init() {
     strokeColor.addEventListener("input", changeStrokeStyle, false);
     canvasColor.addEventListener("input", changeBackgroundColor, false);
     
-    undoTextBtn.addEventListener("click", deleteLastText, false);
-    deleteLastStep.addEventListener("click", deleteStep, false);
+    undoTextBtn.addEventListener('mousedown', deleteLastText, false);
+    deleteLastStep.addEventListener('mousedown', deleteStep, false);
     fontSize.addEventListener("input", changeFontSize, false);
     
    
-    clearCanvas.addEventListener("click", eraseCanvas, false);
+    clearCanvas.addEventListener('mousedown', eraseCanvas, false);
     takeSnapshot();
 }
 
@@ -265,31 +265,5 @@ function init() {
 window.addEventListener('load', init, false);
 
 
- initTouch();
 
-function initTouch() {
-  document.addEventListener("touchstart", touchHandler, true);
-  document.addEventListener("touchmove", touchHandler, true);
-  document.addEventListener("touchend", touchHandler, true);
-  document.addEventListener("touchcancel", touchHandler, true);   
-  }
-  function touchHandler(event)
-  {
-  var touches = event.changedTouches,
-  first = touches[0],
-  type = "";
-  switch(event.type)
-  {
-  case "touchstart": type = "mousedown"; break;
-  case "touchmove":  type="mousemove"; break;        
-  case "touchend":   type="mouseup"; break;
-  default: return;
-  }
-  var simulatedEvent = document.createEvent("MouseEvent");
-   simulatedEvent.initMouseEvent(type, true, true, window, 1,
-                      first.screenX, first.screenY,
-                      first.clientX, first.clientY, false,
-                      false, false, false, 0/*left*/, null);
-  first.target.dispatchEvent(simulatedEvent); 
-  event.preventDefault();
-   }
+
